5ca43b4bc965afb667bc421c358fe0b98b8d10f9
SaimaS <saima.salonen@gmail.com>
2024-03-20 23:28:25 +0200

    Lisätty yksittäisen kurssin muotoilusta huolehtiva komponentti Course, joka sisältää aikaisemman version muut komponentit 2.1 valmis

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
 import { useState } from "react";
+import "./Reset.css";
 import "./App.css";
+import Course from "./components/Course";
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
-}
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    id: 1,
+    parts: [
+      {
+        name: "Fundimentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+  return (
+    <>
+      <Course course={course} />
+    </>
+  );
+};
 
 export default App;
